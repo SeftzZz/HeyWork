c2b3f74d5a45fb113501b54e0aa272e6236d5376
SeftZzz <yerblues6@gmail.com>
2026-02-02 19:36:46 +0700

    bypass login DEV

--- android/app/src/main/java/com/heywork/salam/MainActivity.java
+++ android/app/src/main/java/com/heywork/salam/MainActivity.java
@@ -1,5 +1,21 @@
 package com.heywork.salam;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    WebView webView = this.bridge.getWebView();
+    WebSettings settings = webView.getSettings();
+
+    // 🔓 BYPASS MIXED CONTENT (DEV ONLY)
+    settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+  }
+}
