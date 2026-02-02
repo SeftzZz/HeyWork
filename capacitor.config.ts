f525a633ff36bb988a2d3ecf56222e96b556c6a2
SeftZzz <yerblues6@gmail.com>
2026-02-02 19:16:40 +0700

    add android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HeyWork',
+  webDir: 'www'
+};
+
+export default config;
